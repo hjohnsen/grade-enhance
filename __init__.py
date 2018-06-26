0981dd30e857b60cc470dd59e811bcad93e42cdc
H Johnsen <35159259+hjohnsen@users.noreply.github.com>
2018-06-26 13:37:51 -0700

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-from project import views
-
-# upload_folder = "project/temp/"
-# allowed_extensions = ["pdf"]
-#
-#
-# app.config['upload_folder'] = upload_folder
